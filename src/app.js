31ccbcff0ee0b0eee5fc0816aac47e959fe1561b
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2024-04-05 11:50:25 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ export var App = () => {
         alignItems: 'center',
       }}
     >
-      What is causing this?
+      Rocket
       <img
         src='https://github.com/concrete-utopia/utopia/blob/master/editor/resources/editor/pyramid_fullsize@2x.jpg?raw=true'
         alt='Utopia logo'
